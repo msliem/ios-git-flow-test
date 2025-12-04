5bdc562bb28397b997b38bd0535eba72186a50a8
msliem <m.sliem@otas.net>
2025-12-04 11:31:59 +0200

    [ios] wip: [FeatureThree] Add feature three

---
+++ SwiftUIThinking/TextInputView.swift
@@ -0,0 +1,18 @@
+//
+//  TextInputView.swift
+//  SwiftUIThinking
+//
+//  Created by Mohamed Sliem on 04/12/2025.
+//
+
+import SwiftUI
+
+struct TextInputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextInputView()
+}
